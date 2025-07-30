2f25b62d268f425de8ef58631baa30e4b21dd9e1
MehdiElrafi <mehdi.elrafi@gmail.com>
2025-07-30 22:08:36 +0100

    Install Vite + Eslint

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
-export default App;
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Pixel Perfect Pizzas"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));

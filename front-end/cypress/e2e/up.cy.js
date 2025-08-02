83347ff3bac369bdf67c074d038e23a975b2a6e5
MehdiElrafi <mehdi.elrafi@gmail.com>
2025-08-02 16:17:59 +0100

    update main.yml and failed tests

--- front-end/cypress/e2e/up.cy.js
+++ front-end/cypress/e2e/up.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit("/");
-    cy.location('pathname').should('eq', '/');
+    cy.location('pathname').should('eq', '/test');
   })
 })
